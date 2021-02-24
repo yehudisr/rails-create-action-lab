ee9e09747efc861a9a480dd802dbf9eb246b0a93
yehudisr <77118499+yehudisr@users.noreply.github.com>
2021-02-24 09:35:12 -0500

    commit tree 40ba1a5a9332257f395c2a069d8b6b689193e182

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Student.new(first_name:"yehudis", last_name:"raitman")
+Student.new(first_name:"green", last_name:"peas")
